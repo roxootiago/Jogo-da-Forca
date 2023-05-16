c1aeb5b20bc4c718a83e7865ad30d8fffa05dc8b
Tiago Rôxo <83374845+roxootiago@users.noreply.github.com>
2023-05-16 01:09:53 -0300

    final version 😎

--- Forca/Forca.py
+++ Forca/Forca.py
@@ -1,8 +1,17 @@
-from champsGenerator import champs
+from random import choice
+import cassiopeia as cass
 
 
 def Forca():
-    champsRand = champs().upper()
+    def champsGenerator():
+        champions = cass.get_champions(region="BR")
+        random_champion = choice(champions)
+        return random_champion.name, random_champion.title
+
+    name = champsGenerator()
+    nameChamp = name[0]
+    title = name[1]
+    champsRand = nameChamp.upper()
     emptyList = []
 
     def continuar():
@@ -12,6 +21,7 @@ def Forca():
             Forca()
 
         else:
+
             print("Encerrando o jogo...")
 
     def tentativa():
@@ -28,8 +38,23 @@ def Forca():
     print(" ---x--- JOGO DA FORCA ---x---")
     print("--- ADIVINHE O NOME DO CHAMP ---")
 
-    print(champsRand)
-    print(f"O campeão tem {len(champsRand)} letras")
+    while True:
+        nivel = int(input(
+            "Escolha uma opção de nível:\n(1)Fácil = 10 tentativas\n(2)Médio = 5 tentativas\n(3)Difícil = 3 "
+            "tentativas\nDigite sua opção: "))
+        if nivel == 1:
+            tentativas = 10
+            break
+        elif nivel == 2:
+            tentativas = 5
+            break
+        elif nivel == 3:
+            tentativas = 3
+            break
+        else:
+            print("Digite um nível válido!")
+
+    print(f"Dica: {title}\nO campeão tem {len(champsRand)} letras")
 
     for k in range(len(champsRand)):
         emptyList.append("_")
@@ -45,28 +70,45 @@ def Forca():
             emptyList[t] = "."
 
     def jogar():
+        count = 1
+        while count <= tentativas:
 
-        for y in range(len(champsRand)):
-            print(emptyList[y], end=" ")
-        print()
+            print(f"Tentativas: {count} de {tentativas}")
+            for y in range(len(champsRand)):
+                print(emptyList[y], end=" ")
+            print()
 
-        kick = input("Digite uma letra ou 'tentar' para tentar a palavra: ").upper()
+            kick = input("Digite uma letra ou 'tentar' para tentar a palavra: ").upper()
 
-        if kick == "TENTAR":
-            tentativa()
+            if kick == "TENTAR":
+                tentativa()
+                break
+            if kick in emptyList:
+                print(f"'{kick}' ja se encontra na palavra!\nDigite uma letra diferente")
+            elif len(kick) > 1:
+                print("Digite apenas uma letra ou 'tentar'!")
 
-        elif kick != "TENTAR":
-            for z in range(len(champsRand)):
-                if champsRand[z] == kick:
-                    emptyList[z] = kick
-                    if listChamp == emptyList:
-                        print(f"Parabéns! Você completou a palavra\nCampeão selecionado: {champsRand}")
-                        continuar()
-                elif kick not in champsRand:
-                    print("esta letra não está presente")
+            elif kick != "TENTAR":
+                letra_encontrada = False
+                for z in range(len(champsRand)):
+                    if champsRand[z] == kick:
+                        emptyList[z] = kick
+                        letra_encontrada = True
+
+                if emptyList == listChamp:
+                    print(f"Você completou a palavra\nCampeão selecionado: {nameChamp.capitalize()}: {title}")
+                    continuar()
                     break
-            jogar()
+
+                if not letra_encontrada:
+                    print(f"A palavra não possui a letra '{kick}'")
+                    count += 1
+                if count > tentativas:
+                    print(f"Você perdeu!")
+                    continuar()
+                    break
 
     jogar()
 
-Forca()
+
+
